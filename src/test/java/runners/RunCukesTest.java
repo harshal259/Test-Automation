07ed27b783e7e61c0552ccd85ab0f2c86693562b
harshal259 <harshal259@github.com>
2020-01-18 14:15:06 +0530

    Updated Test Runner Config

--- src/test/java/runners/RunCukesTest.java
+++ src/test/java/runners/RunCukesTest.java
@@ -6,7 +6,9 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "/var/lib/jenkins/workspace/MyFW/src/test/resources/features",
+//        features = "/var/lib/jenkins/workspace/MyFW/src/test/resources/features",
+//        features = "C:\\Program Files (x86)\\Jenkins\\workspace\\Test-Automation\\src\\test\\resources\\features",
+        features = "src\\test\\resources\\features",
         glue = {"stepdefs", "utils"},
         plugin = {"pretty", "json:target/reports/cucumber.json"},
         tags = "@test"
